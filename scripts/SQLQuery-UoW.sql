04dd5ba67726e57198f82ab2f03eb165ff7a8662
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-02 13:28:07 -0300

    Change .Sql Script

--- scripts/SQLQuery-UoW.sql
+++ scripts/SQLQuery-UoW.sql
@@ -5,5 +5,5 @@ Select * from Pessoa
 
 -- delete from Pessoa where VooId = 'C05ACEB7-1667-4D8F-BD9E-400984609721';
 -- delete from Voo where Id = 'C05ACEB7-1667-4D8F-BD9E-400984609721';
--- insert Voo (Id, Codigo, Nota, Capacidade, Disponibilidade) values ('C05ACEB7-1667-4D8F-BD9E-400984609721', '101 - Rio/Miami', 'Saída às 10:34h. UTC-Brazilian', 4, 4);
+-- insert Voo (Id, Codigo, Nota, Capacidade, Disponibilidade) values ('C05ACEB7-1667-4D8F-BD9E-400984609721', '101 - Rio/Miami', 'Saída às 10:34h. Horário de Brasília', 4, 4);
 -- Update Voo set Capacidade = 4, Disponibilidade = 4 where Id = 'C05ACEB7-1667-4D8F-BD9E-400984609721'; 
