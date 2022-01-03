d2fe50a325b849d05b63f823ec1d231b2c037877
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-03 09:55:20 -0300

    Controlando o retorno da request com uso de ActionResult e capturando o erro.

--- src/Cooperchip.DiretoAoPonto.Api/Controllers/PessoaController.cs
+++ src/Cooperchip.DiretoAoPonto.Api/Controllers/PessoaController.cs
@@ -6,7 +6,8 @@ using UnitOfWorkExample.Data.Repositories;
 namespace Cooperchip.DiretoAoPonto.Uow.Controllers
 {
     [ApiController]
-    [Route("[Controller]")]
+    //[Route("[Controller]")]
+    [Route("api/pessoa")]
     public class PessoaController : Controller
     {
 
@@ -20,7 +21,13 @@ namespace Cooperchip.DiretoAoPonto.Uow.Controllers
             _vooRepo = vooRepo;
         }
 
-        [HttpPost]
+        /// <summary>
+        /// Deixando o erro estourar na tela.
+        /// E é gerado um StatusCode 500, o que é um erro.
+        /// </summary>
+        /// <param name="pessoa"></param>
+        /// <returns></returns>
+        [HttpPost("adicionar-pessoa")]
         public async Task<PessoaDTO> AdicionarPessoa([FromBody] PessoaRequest pessoa)
         {
             var pessoaModel = new Pessoa
@@ -28,12 +35,9 @@ namespace Cooperchip.DiretoAoPonto.Uow.Controllers
                 VooId = pessoa.VooId,
                 Nome = pessoa.Nome
             };
-
             await _pessoaRepo.AdicionarPessoa(pessoaModel);
             await _vooRepo.DecrementarPessoa(pessoa.VooId);
-
             await _pessoaRepo.Commit();
-
             var pessoaDto = new PessoaDTO
             {
                 VooId = pessoaModel.VooId,
@@ -41,8 +45,36 @@ namespace Cooperchip.DiretoAoPonto.Uow.Controllers
                 Id = pessoaModel.Id
             };
             return pessoaDto;
-
-
         }
+
+        [HttpPost("add-pessoa")]
+        public async Task<ActionResult<PessoaDTO>> AddPessoa(PessoaRequest pessoa)
+        {
+            var pessoaModel = new Pessoa
+            {
+                VooId = pessoa.VooId,
+                Nome = pessoa.Nome
+            };
+
+            try
+            {
+                await _pessoaRepo.AdicionarPessoa(pessoaModel);
+                await _vooRepo.DecrementarPessoa(pessoa.VooId);
+                await _pessoaRepo.Commit();
+                var pessoaDto = new PessoaDTO
+                {
+                    VooId = pessoaModel.VooId,
+                    Nome = pessoaModel.Nome,
+                    Id = pessoaModel.Id
+                };
+                return Ok(pessoaDto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
     }
 }
